e48d64602e3154215eeb478ac56d8a45b750aac8
00039425 <00039425@uca.edu.sv>
2025-06-04 12:11:25 -0600

    Ejercicio while hecho

---
+++ EjericicioWHILE.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num = 30, intentos = 0, numI = 0;
+    
+    do{
+        
+        if(intentos == 5){
+            cout << "has fallado." << endl;
+            break;
+        }
+        
+        cout << "Adivine el numero del 1 al 50. Tienes 5 intentos: " ;
+        cin >> numI;
+        
+        if(num == numI){
+            cout << "Adivinaste el numero, felicidades" << endl;
+            break;
+        }else{
+            cout << "Vuelve a intentarlo." << endl;
+            intentos++;
+            cout << "Intentos: " << intentos << endl;
+        }
+    }while(num != numI || intentos != 5);
+    
+}
